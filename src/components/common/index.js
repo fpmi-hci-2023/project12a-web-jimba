17c788e5041831d7110a98ea7482e3c87872a28d
Foltrex <fpm.chernenkAV@bsu.by>
2023-11-26 21:51:06 +0300

    Add export component's module

---
+++ src/components/common/index.js
@@ -0,0 +1,17 @@
+import Input from './Input';
+import Button from './Button';
+import BackButton from './BackButtonWithProps';
+import BackButtonWithContext from './BackButtonWithContext';
+import ButtonWithClickEvent from './ButtonWithClickEvent';
+import ErrorPage from './ErrorPage';
+import ToastComponent from './ToastComponent';
+
+export{
+    Input,
+    Button,
+    BackButton,
+    BackButtonWithContext,
+    ButtonWithClickEvent,
+    ErrorPage,
+    ToastComponent
+};
